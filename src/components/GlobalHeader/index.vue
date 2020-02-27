071d05278416370fc63773b10ee089754969d628
Amfine <kittinut.l@ku.th>
2020-02-27 21:45:30 +0700

    update

--- src/components/GlobalHeader/index.vue
+++ src/components/GlobalHeader/index.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="header">
-    <div>Jongme</div>
-    <div>Jongme</div>
-    <div class="right">Jongme</div>
+    <a-row style="margin: 0">
+      <a-col :span="8">
+        <div class="mini_icon">JM</div>
+      </a-col>
+      <a-col :span="16" class="right">
+        <a-dropdown>
+          <a class="ant-dropdown-link" href="#">
+            Jongme
+            <a-icon type="down" />
+          </a>
+
+          <a-menu slot="overlay">
+            <a-menu-item>
+              <a href="javascript:;">Select page</a>
+            </a-menu-item>
+            <a-menu-item>
+              <a href="javascript:;">Setting</a>
+            </a-menu-item>
+            <a-menu-item>
+              <a href="javascript:;">Logout</a>
+            </a-menu-item>
+          </a-menu>
+        </a-dropdown>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
@@ -10,6 +32,6 @@
 export default {};
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 @import "./index.less";
 </style>
